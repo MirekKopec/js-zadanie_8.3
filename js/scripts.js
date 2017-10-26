2cf4e3058f601543b156c0c6251a46ec49ce654f
MirekKopec <mkopec95@gmail.com>
2017-10-26 21:34:53 +0200

    added comments in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,36 @@
 // scripts.js
+
+// todo: why alert is loading before html content?
+
 document.addEventListener("DOMContentLoaded", function() {
+
 var title = document.getElementsByTagName('h1')[0].textContent;
+
 var name = prompt('Enter your name');
+
 if (name =='') {
+
 name = 'Anonymus';
+
 }
+
 alert('Hello, ' + name);
+
 var tekst = title + ', ' + name + '!';
+
 document.getElementsByTagName('h1')[0].textContent = tekst;
 
 function windowAlert() {
+
     alert('Nothing here yet...under construction;)');
+
 }
+
 document.getElementsByTagName('button')[0].addEventListener('click', windowAlert);
 
+/*
 console.log('Hello, ' + name);
+
 console.log(tekst);
+*/
 });
